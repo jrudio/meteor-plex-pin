f86ad691b860b4b1cc718c86e0fc415e7bf770af
jrudio <justinjrudio@gmail.com>
2015-05-01 02:46:05 -0700

    Bumped to 0.0.5

--- package.js
+++ package.js
@@ -1,22 +1,38 @@
 Package.describe({
+  // name: 'jrudio:plex-pin',
   name: 'jrudio:plex-pin',
-  version: '0.0.1',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'A Plex PIN request module for Plex Media Server(PMS) authorization',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jrudio/meteor-plex-pin',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
+Npm.depends({
+  'plex-pin': '0.0.5'
+});
+
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('plexpin.js');
+
+  // Dependencies
+  api.use('http', 'client');
+
+  // Muh Library
+  api.addFiles('lib/server/plexpin.js', 'server');
+  api.addFiles('lib/client/plexpin.js', 'client');
+
+  // Promise Library
+  api.addFiles('compatibility/bluebird.min.js', 'client');
+
+  api.export('PlexPin');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('jrudio:plex-pin');
-  api.addFiles('plexpin-tests.js');
+  api.addFiles('test/plexpin-tests.js');
 });
